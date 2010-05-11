3f2f6ed15e5d06a0a2f38053070efdded1a95c57
lazewatskyd <lazewatskyd@fb087b99-d080-f783-b68d-3d69b41ef833>
2010-05-11 17:15:59 +0000

    command line tool for testing ptu
    git-svn-id: https://wu-robotics.googlecode.com/svn@768 fb087b99-d080-f783-b68d-3d69b41ef833

---
+++ ptu_control/ptu_action_test.py
@@ -0,0 +1,19 @@
+#!/usr/env/bin python
+import roslib; roslib.load_manifest('ptu_control')
+import rospy
+import ptu_control.msg
+import actionlib
+import sys
+
+def ptu_action_test():
+	client = actionlib.SimpleActionClient('SetPTUState', ptu_control.msg.PtuGotoAction)
+	client.wait_for_server()
+	goal = ptu_control.msg.PtuGotoGoal(pan=float(sys.argv[1]), tilt=float(sys.argv[2]))
+	client.send_goal(goal)
+	client.wait_for_goal_to_finish()
+	return client.get_result()
+	
+if __name__ == '__main__':
+	rospy.init_node('action_test')
+	result = ptu_action_test()
+	print result
