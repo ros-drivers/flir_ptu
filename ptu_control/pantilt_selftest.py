4bcb638e98defc674982f73fb5f6e4e2609433c1
lazewatskyd <lazewatskyd@fb087b99-d080-f783-b68d-3d69b41ef833>
2010-06-17 15:28:08 +0000

    changed pan/tilt ranges based on logitech specs
    git-svn-id: https://wu-robotics.googlecode.com/svn@812 fb087b99-d080-f783-b68d-3d69b41ef833

--- ptu_control/pantilt_selftest.py
+++ ptu_control/pantilt_selftest.py
@@ -6,6 +6,10 @@ from logitech_pantilt.msg import PanTilt
 PAN_STEP   = 2
 TILT_STEP  = 2
 SLEEP_TIME = 0.08
+
+PAN_RANGE = 189
+TILT_RANGE = 102
+
 if __name__ == '__main__':
 	rospy.init_node('pantilt_selftest')
 	pt_pub = rospy.Publisher('/pantilt', PanTilt)
@@ -14,37 +18,37 @@ if __name__ == '__main__':
 	rospy.sleep(0.5)
 		
 	#move CCW
-	for i in range(140/PAN_STEP+1):
+	for i in range(PAN_RANGE/PAN_STEP+1):
 		pt = PanTilt(PAN_STEP, 0, False)
 		pt_pub.publish(pt)
 		rospy.sleep(SLEEP_TIME)
 		
 	#move CW
-	for i in range(140/PAN_STEP+1):
+	for i in range(PAN_RANGE/PAN_STEP+1):
 		pt = PanTilt(-PAN_STEP, 0, False)
 		pt_pub.publish(pt)
 		rospy.sleep(SLEEP_TIME)
 		
 	#move center
-	for i in range(70/PAN_STEP-1):
+	for i in range(PAN_RANGE/2/PAN_STEP-1):
 		pt = PanTilt(PAN_STEP, 0, False)
 		pt_pub.publish(pt)
 		rospy.sleep(SLEEP_TIME)
 		
 	#move up
-	for i in range(60/TILT_STEP+1):
+	for i in range(TILT_RANGE/TILT_STEP+1):
 		pt = PanTilt(0, TILT_STEP, False)
 		pt_pub.publish(pt)
 		rospy.sleep(SLEEP_TIME)
 	
 	#move down
-	for i in range(60/TILT_STEP+1):
+	for i in range(TILT_RANGE/TILT_STEP+1):
 		pt = PanTilt(0, -TILT_STEP, False)
 		pt_pub.publish(pt)
 		rospy.sleep(SLEEP_TIME)
 		
 	#move center
-	for i in range(30/TILT_STEP-1):
+	for i in range(TILT_RANGE/2/TILT_STEP-1):
 		pt = PanTilt(0, TILT_STEP, False)
 		pt_pub.publish(pt)
 		rospy.sleep(SLEEP_TIME)
