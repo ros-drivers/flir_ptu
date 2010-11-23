345b15d25d44234eb8e68fed09caa6c923243c34
lazewatskyd <lazewatskyd@fb087b99-d080-f783-b68d-3d69b41ef833>
2010-11-23 19:55:02 +0000

    now determine when movement is done, and keep track of ptu state internally
    git-svn-id: https://wu-robotics.googlecode.com/svn@982 fb087b99-d080-f783-b68d-3d69b41ef833

--- ptu46/ptu_action_server.py
+++ ptu46/ptu_action_server.py
@@ -5,6 +5,7 @@ import ptu_control.msg
 import actionlib
 from sensor_msgs.msg import JointState
 import threading
+import numpy as np
 
 class PTUControl(object):
 	pan      = 0
@@ -19,20 +20,25 @@ class PTUControl(object):
 		self.tsmax = rospy.get_param('/ptu/max_tilt_speed', 140.0)
 		self.psmin = rospy.get_param('/ptu/min_pan_speed' , 4.0)
 		self.psmax = rospy.get_param('/ptu/max_pan_speed' , 140.0)
+		self.pstep = rospy.get_param('/ptu/pan_step', 0.00089759763795882463)
+		self.tstep = rospy.get_param('/ptu/tilt_step', 0.00089759763795882463)
 
 		# setup the subscribers and publishers
-		self.ptu_pub = rospy.publisher('cmd')
+		rospy.Subscriber('state', JointState, self.cb_ptu_state)
+		self.ptu_pub = rospy.Publisher('cmd', JointState)
 		self.as_goto = actionlib.SimpleActionServer('SetPTUState', \
 		     ptu_control.msg.PtuGotoAction, execute_cb=self.cb_goto)
 	
 	def cb_goto(self, msg):
-		#self.state_lock.acquire()
-		pan, tilt, pan_vel, tilt_vel = msg.pan, msg.tilt, msg.pan_vel, msg.tilt_vel
+		
+		pan, tilt, pan_vel, tilt_vel = np.radians((msg.pan, msg.tilt, msg.pan_vel, msg.tilt_vel))
 
 		if not pan_vel:
-			pan_vel = psmax
+			pan_vel = self.psmax
 		if not tilt_vel:
-			tilt_vel = tsmax
+			tilt_vel = self.tsmax
+
+		#tilt_vel = pan_vel = 1
 		
 		msg_out = JointState()
 		msg_out.header.stamp = rospy.Time.now()
@@ -40,11 +46,28 @@ class PTUControl(object):
 		msg_out.position = [pan, tilt]
 		msg_out.velocity = [pan_vel, tilt_vel]
 		self.ptu_pub.publish(msg_out)
-
+		# wait for it to get there
+		wait_rate = rospy.Rate(10)
+		while not self._at_goal((msg.pan, msg.tilt)) and not rospy.is_shutdown():
+			wait_rate.sleep()
 		result = ptu_control.msg.PtuGotoResult()
-		# TODO this is WRONG and needs to be fixed (these values aren't necessarily right)
-		result.state.position = [pan, tilt]
+		result.state.position = self._get_state()
 		self.as_goto.set_succeeded(result)
+		
+
+	def _at_goal(self, goal):
+		return all(np.abs(np.array(goal) - (self.pan, self.tilt)) <= np.degrees((self.pstep, self.tstep)))
+
+	def cb_ptu_state(self, msg):
+		self.state_lock.acquire()
+		self.pan, self.tilt = np.degrees(msg.position)
+		self.state_lock.release()
+
+	def _get_state(self):
+		self.state_lock.acquire()
+		pt = self.pan, self.tilt
+		self.state_lock.release()
+		return pt
 
 if __name__ == '__main__':
 	rospy.init_node('ptu46_action_server')
