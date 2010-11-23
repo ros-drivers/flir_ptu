8a19294496130ff90a89e9c8eb93f83dcfc5e7d2
lazewatskyd <lazewatskyd@fb087b99-d080-f783-b68d-3d69b41ef833>
2010-11-23 16:58:25 +0000

    changes for getting angles
    git-svn-id: https://wu-robotics.googlecode.com/svn@979 fb087b99-d080-f783-b68d-3d69b41ef833

--- ptu_control/src/ptu_control/geometry.py
+++ ptu_control/src/ptu_control/geometry.py
@@ -3,6 +3,10 @@
 import roslib; roslib.load_manifest('ptu_control')
 from tf.transformations import euler_from_matrix
 import numpy as np
+import cv
+from missouri_telepresence import surf_helper
+
+X_AXIS, Y_AXIS, Z_AXIS = range(3)
 
 def euler_from_homography(H, K):
 	# apply the calibration matrix to H, then take the QR decomposition
@@ -22,12 +26,19 @@ def euler_from_homography(H, K):
 	return euler_from_matrix(z)
 	
 def y_rot_from_homography(H, K):
+	print "r = \n %s" % str(K.I*H*K)
 	q, r = np.linalg.qr(K.I*H*K)
-	return np.average([np.arccos(q[0,0]),
-					   np.arcsin(q[0,2]),
-					  -np.arcsin(q[2,0]),
-					   np.arccos(q[2,2])
-					])
+	# q, r = np.linalg.qr(K*H*K.I)
+	# return np.average([np.arccos(q[0,0]),
+	# 				   np.arcsin(q[0,2]),
+	# 				  -np.arcsin(q[2,0]),
+	# 				   np.arccos(q[2,2])
+	# 				])
+	return [np.arccos(q[0,0]),
+		    np.arcsin(q[0,2]),
+		   -np.arcsin(q[2,0]),
+		    np.arccos(q[2,2])
+		   ]	
 
 if __name__ == '__main__':
 	# Homography
@@ -44,3 +55,30 @@ if __name__ == '__main__':
 		[        0,        0, 1.0000]]
 	)
 	print np.degrees(euler_from_homography(H, K))
+	
+def get_surf_features(image):
+	image = cv.GetMat(image)
+	img_sz = cv.GetSize(image)
+
+	eig_image = cv.CreateImage(img_sz, cv.IPL_DEPTH_32F, 1)
+	tmp_image = cv.CreateImage(img_sz, cv.IPL_DEPTH_32F, 1)
+	
+	return cv.ExtractSURF(image, None, cv.CreateMemStorage(), (0, 200, 3, 1))
+	
+def rotation_from_images(im1, im2, K, axis=None):
+	feat_1 = get_surf_features(im1)
+	feat_2 = get_surf_features(im2)
+	
+	H = cv.CreateMat(3, 3, cv.CV_32FC1)
+	feat, pts = surf_helper.getPointLists(feat_1[0], feat_1[1], feat_2[0], feat_2[1])
+	cv.FindHomography(np.asarray(pts), np.asarray(feat), H, cv.CV_RANSAC)
+	
+	
+	if axis == X_AXIS:
+		pass
+		
+	if axis == Y_AXIS:
+		return y_rot_from_homography(H, K)
+		
+	if axis == Z_AXIS:
+		pass
